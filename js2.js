dbebe9ba90db4874cf119d10c4b6e394ff7fee0b
Valentyna13 <57989772+Valentyna13@users.noreply.github.com>
2020-12-02 14:21:30 +0300

    Update js2.js

--- js2.js
+++ js2.js
@@ -1,17 +1,14 @@
 function changeDesign(input) {
 	var id = input.id;
-	var elem = document.getElementById("my_form");
-	elem.classList.remove("first","second","third");
 	if (id == "f") {
-		elem.classList.add("first");
 		alert("Первый стиль");
 	}
 	if (id == "s") {
-		elem.classList.add("second");
 		alert("Второй стиль");
+		document.getElementById('bb12').style.color="green";
 	}
 	if (id == "t") {
-		elem.classList.add("third");
 		alert("Третий стиль");
+		document.getElementById('bb12').style.backgroundColor="red";
 	}
 }
